38aeb556d8c0861e650206d687d6ad7dc643324a
sasarky <sasarky@gmail.com>
2016-11-01 01:40:36 +0900

    kyou no bun

--- app/models/room.rb
+++ app/models/room.rb
@@ -12,11 +12,23 @@ class Room < ApplicationRecord
     queues.where({ state: RoomQueue::STATE_QUEUED }).order(:created_at).first
   end
 
-  def add_queue(video)
+  def add_queue(user_id, video)
     queues << RoomQueue.create({
       user_id: 1,
       video_id: video.id,
       state: RoomQueue::STATE_QUEUED,
     })
+  end
+
+  def latest_queue
+    queues.where("state = #{::RoomQueue::STATE_FINISHED}").order('id DESC').limit(1).first
+  end
+
+  def current_queues
+    if latest_queue
+      queues.where("id >= #{latest_queue.id}")
+    else
+      queues.where("state IN (?)", [::RoomQueue::STATE_QUEUED, ::RoomQueue::STATE_PLAYING])
+    end
   end
 end
